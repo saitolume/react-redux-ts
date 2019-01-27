dca0d9b73247f4869e2b58f53160de4910413af4
eclero <eclero357@gmail.com>
2019-01-27 15:43:57 +0900

    feat: add contact page

--- src/components/atoms/Button/index.stories.tsx
+++ src/components/atoms/Button/index.stories.tsx
@@ -3,6 +3,10 @@ import { storiesOf } from '@storybook/react';
 
 import { Button } from './index';
 
-storiesOf('Button', module).add('default', () => {
-  return <Button>default</Button>;
-});
+storiesOf('atoms/Button', module)
+  .add('default', () => {
+    return <Button>default</Button>;
+  })
+  .add('color', () => {
+    return <Button color="red">red</Button>;
+  });
