e43f834411e234d4590d0fd26ad0be24447052f4
saitoeku3 <saitoeku3@gmail.com>
2019-02-02 11:31:00 +0900

    build: add environment plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -31,6 +32,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve('./public/index.html'),
     }),
+    new webpack.EnvironmentPlugin(['OPEN_WEATHER_MAP_KEY']),
   ],
   devServer: {
     historyApiFallback: true,
