57564bc71e86bbf9411f90ef8cce0e7766ec07b9
saitoeku3 <saitoeku3@gmail.com>
2019-02-03 13:28:14 +0900

    feat: connect Heme and qiita reducer

--- src/components/pages/Home/container.ts
+++ src/components/pages/Home/container.ts
@@ -1,25 +1,30 @@
 import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import Home from '.';
-import { AppState, CountState } from '../../../types';
+import { AppState, CountState, QiitaState } from '../../../types';
 import {
   CountAction,
   increment,
   decrement,
   setCount,
 } from '../../../modules/count';
+import { fetchUser } from '../../../modules/qiita';
 
 const homeActions = {
   increment,
   decrement,
   setCount,
+  fetchUser,
 };
 
-export type StateProps = CountState;
+export type StateProps = CountState & QiitaState;
 export type DispatchProps = typeof homeActions;
 
 const mapStateToProps = (state: AppState): StateProps => ({
   value: state.count.value,
+  id: state.qiita.id,
+  name: state.qiita.name,
+  description: state.qiita.description,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch<CountAction>): DispatchProps =>
--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,15 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas';
 import countReducer from './modules/count';
+import qiitaReducer from './modules/qiita';
 
 const rootReducer = combineReducers({
   count: countReducer,
+  qiita: qiitaReducer,
 });
 
-export const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
